2e70a2ccacded6c3b1d89fc30700e20f68059720
ThePriest1 <stas.shilovich@gmail.com>
2021-03-10 15:59:20 +0300

    add oauth2 config

--- controller/src/main/java/com/epam/esm/controller/config/SecurityConfiguration.java
+++ controller/src/main/java/com/epam/esm/controller/config/SecurityConfiguration.java
@@ -1,11 +1,13 @@
 package com.epam.esm.controller.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -21,15 +23,18 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                /// TODO: 02.03.2021 fix
-                .authorizeRequests().anyRequest().authenticated()
-                .and().httpBasic()
-                .and().sessionManagement().disable();
+//        http
+//                .csrf().disable()
+//                /// TODO: 02.03.2021 fix
+//                .authorizeRequests().anyRequest().authenticated()
+////                .and().httpBasic()
+//                .and().sessionManagement().disable();
+        http.authorizeRequests().anyRequest().authenticated().and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.NEVER);
     }
 
     @Override
+    @Autowired
     protected void configure(AuthenticationManagerBuilder builder) throws Exception {
         builder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
